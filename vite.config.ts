b75f28d88d42add7f5ee055a1a832a04f0187f01
yc9954 <alexyc0506@gmail.com>
2026-01-17 21:15:52 +0900

    cache clean

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 5173,
+    host: true,
+    open: true,
+    strictPort: false,
+  },
 })
